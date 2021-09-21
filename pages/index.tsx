ec480ca0c55d78ba6473922797d1282db25df3f4
wajima <mikkywaji@icloud.com>
2021-09-21 17:39:38 +0900

    テストコミット

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,6 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -21,8 +20,6 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
