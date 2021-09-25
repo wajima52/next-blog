0cf84447c26ee85156b314146fa017f00e6143ac
wajima <mikkywaji@icloud.com>
2021-09-26 08:42:56 +0900

    PC版のナビバーのレイアウトを調整

--- components/Organisms/Header.tsx
+++ components/Organisms/Header.tsx
@@ -5,10 +5,14 @@ import PcNav from "../Molecules/PcNav"
 export const Header: React.FC = () => {
   return (
     <>
-      <header className="w-full flex bg-white  items-center justify-between flex-wrap bg-teal py-6 px-6 sm:px-20 shadow-md ">
-        <div className="items-center flex-no-shrink mr-6">Site Logo</div>
-        <PcNav />
-        <SpNav />
+      <header className="w-full bg-white flex-wrap bg-teal py-6 px-6 sm:px-20 shadow-md ">
+        <div
+          className={`flex items-center justify-between max-w-2xl xl:max-w-4xl mx-auto`}
+        >
+          <div className="items-center flex-no-shrink mr-6">Site Logo</div>
+          <PcNav />
+          <SpNav />
+        </div>
       </header>
     </>
   )
